6032898e66190d2be75b8d09fb3c78fee0b64285
ggauravgpt <57315411+ggauravgpt@users.noreply.github.com>
2019-12-01 18:45:27 +0530

    Update PrintConsole.java

--- parkingspot/src/main/java/parkingspot/Main/PrintConsole.java
+++ parkingspot/src/main/java/parkingspot/Main/PrintConsole.java
@@ -5,27 +5,29 @@ public class PrintConsole {
 	public static void printUsage()
 	{
 		StringBuffer buffer = new StringBuffer();
-		buffer = buffer.append(
-				"--------------Please Enter one of the below commands. {variable} to be replaced -----------------------")
-				.append("\n");
-		buffer = buffer.append("A) For creating parking lot of size n               ---> create_parking_lot {capacity}")
+
+		buffer = buffer.append("1) Command For creating parking lot of size n       --> create_parking_lot {size}")
 				.append("\n");
 		buffer = buffer
-				.append("B) To park a car                                    ---> park <<car_number>> {car_clour}")
+				.append("2) Command For parking a car                              ---> park <<car_number>> {color}")
 				.append("\n");
-		buffer = buffer.append("C) Remove(Unpark) car from parking                  ---> leave {slot_number}")
+		buffer = buffer.append("3) Command For Removeing (Unparking ) car from parking            ---> leave {slot_number}")
 				.append("\n");
-		buffer = buffer.append("D) Print status of parking slot                     ---> status").append("\n");
+		buffer = buffer.append("4) Command For Printing status of parking slot                     ---> status").append("\n");
 		buffer = buffer.append(
-				"E) Get cars registration no for the given car color ---> registration_numbers_for_cars_with_color {car_color}")
-				.append("\n");
-		buffer = buffer.append(
-				"F) Get slot numbers for the given car color         ---> slot_numbers_for_cars_with_color {car_color}")
+				"5) Command to Get cars registration no for the given car color ---> registration_numbers_for_cars_with_color {car_color}")
 				.append("\n");
 		buffer = buffer.append(
-				"G) Get slot number for the given car number         ---> slot_number_for_registration_number {car_number}")
+				"6) Command to Get slot numbers for the given car color         ---> slot_numbers_for_cars_with_color {car_color}")
 				.append("\n");
+		buffer = buffer.append(
+				"7) Command to Get slot number for the given car number         ---> slot_number_for_registration_number {car_number}")
+				.append("\n");
+		buffer = buffer.append(
+				"To end Execution Enter 'exit or 0' ").append("\n");
+
 		System.out.println(buffer.toString());
+		
 	}
 
 }
